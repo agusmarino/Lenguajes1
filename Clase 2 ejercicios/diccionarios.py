31daf7fcc0d5540d75db009af5bc09dbe34c6209
agusmarino <agustinezequiel.marino@ucalpvirtual.edu.ar>
2025-09-19 17:50:20 -0300

    subida de ejercicios

--- Clase 2 ejercicios/diccionarios.py
+++ Clase 2 ejercicios/diccionarios.py
@@ -4,18 +4,60 @@
 #Promedio general, mejor estudiante, y los/as que están por debajo del promedio.
 #Construir un diccionario {palabra: longitud} a partir de una frase.
 
-notas = {}
+notas = {
+    "agus": 10,
+    "nico": 8,
+    "fran": 4,
+    "juli": 0
+}
 
 print("para terminar de ingresar nombre = fin")
 
-while True:
-    nombre = input("Ingrese el nombre : ")
+# while True:
+#     nombre = input("Ingrese el nombre : ")
     
-    if nombre == "fin":
-        break
+#     if nombre == "fin":
+#         break
     
-    edad = input("Ingrese la edad: ")
-    notas[nombre] = edad
+#     nota = int(input("Ingrese la nota: "))
+#     notas[nombre] = nota
     
 print(notas)
 
+sumaNotas = 0
+max = -1
+estudianteMejor = "def"
+
+n = notas.values()
+
+for i in n:
+    sumaNotas += i
+
+promedio = sumaNotas / len(n)
+print(promedio)
+
+listaMenosPromedio = []
+
+h = notas.items()
+
+for i in h:
+    if i[1] < promedio:
+        listaMenosPromedio.append(i)
+        
+print("el promedio general es: ", promedio)
+print("el mejor promedio es: ", estudianteMejor,"su nota es: ",max)
+print("los alumnos con nota menor al promedio son: ",listaMenosPromedio)
+
+
+#.....................................
+#....................................-
+
+frase = input("Ingrese una frase: ")
+diccionarioFrase = {}
+
+for i in frase.split():
+    palabra = i
+    longitud = len(palabra)
+    diccionarioFrase[palabra] = longitud
+    
+print(diccionarioFrase)
--- Clase 2 ejercicios/funciones.py
+++ Clase 2 ejercicios/funciones.py
@@ -4,6 +4,32 @@
 #Escribir contar_vocales(cadena) que retorne un diccionario con la cuenta de cada vocal.
 #Escribir normalizar_palabras(frase) que retorne una lista de palabras sin signos de puntuación y en minúsculas.
 
-palabra = input("ingrese una palabra: ")
+import string
 
-def es_palindromo(palabra):
+def contar_vocales(cadena):
+    cadena = cadena.lower()
+    vocales = "aeiou"
+    conteo = {v: 0 for v in vocales}  # inicializa diccionario con 0
+    
+    for letra in cadena:
+        if letra in conteo:
+            conteo[letra] += 1
+    return conteo
+
+def normalizar_palabras(frase):
+    # pasar a minúsculas
+    frase = frase.lower()
+    # quitar signos de puntuación
+    for signo in string.punctuation:
+        frase = frase.replace(signo, "")
+    # dividir en palabras
+    return frase.split()
+
+def normalizar_palabras(frase):
+    # pasar a minúsculas
+    frase = frase.lower()
+    # quitar signos de puntuación
+    for signo in string.punctuation:
+        frase = frase.replace(signo, "")
+    # dividir en palabras
+    return frase.split()
